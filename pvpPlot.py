1f40f87dd62f0add13072b2a397e62c6441d7a0b
mding010705 <88820610+mding010705@users.noreply.github.com>
2021-08-18 01:47:38 -0400

    Update pvpPlot.py
    comments

--- pvpPlot.py
+++ pvpPlot.py
@@ -2,6 +2,7 @@ import profitvprob as pvp
 import matplotlib.pyplot as plt
 import numpy as np
 
+#gathering info on first roll to form graph
 print('What is your roll? (ex. 13362)')
 roll= list(input())
 for i in range (len(roll)):
@@ -18,6 +19,7 @@ print(pvp.FrOKscore(roll))
 profit=[]
 prob=[]
 labels=[]
+#sorts out the zero's from the data from profitvprob, condenses the separate lists into profit array (x values) and prob array (y values), and makes label for point
 for i in range (len(pvp.USScores(roll)[0])):
     if pvp.USScores(roll)[0][i]!=0:
         profit.append(pvp.USScores(roll)[0][i])
@@ -43,10 +45,14 @@ profit.append(pvp.yahtzeeScore(yahtzee, roll)[0])
 prob.append(pvp.yahtzeeScore(yahtzee, roll)[1])
 labels.append('Yahtzee')
 
+#possible plot point colours
 colours=['red','orange','gold','lawngreen','forestgreen','turquoise','teal','cyan','dodgerblue','gray','navy','darkorchid','darkmagenta','magenta','deeppink','palevioletred']
+
+#placing each plot point individually so they can have distinct labels
 for l in range (len(labels)):
     plt.plot(profit[l], prob[l], 'o', color=colours[l],label=labels[l])
     
+#gives and plots linear regression of all points
 print(np.polyfit(profit, prob, 1).tolist())
 m = np.polyfit(profit, prob, 1).tolist()[0]
 b = np.polyfit(profit, prob, 1).tolist()[1]
@@ -55,9 +61,11 @@ for p in range (len(profit)):
     reg.append(profit[p]*m+b)
 plt.plot(profit,reg, label='Baseline')
 
+#taking Yahtzee off of data arrays since it is often an outlier that is not considered when trying to score so a new trendline can be made
 profit0=list(profit[:-1])
 print(profit0)
 prob0=list(prob[:-1])
+#gives and plots linear regression excluding Yahtzee
 print(np.polyfit(profit0, prob0, 1).tolist())
 m0 = np.polyfit(profit0, prob0, 1).tolist()[0]
 b0 = np.polyfit(profit0, prob0, 1).tolist()[1]
@@ -65,6 +73,7 @@ reg0=[]
 for p in range (len(profit0)):
     reg0.append(profit0[p]*m0+b0)
 plt.plot(profit0,reg0, label='Baseline (without Yahtzee)')
+
 # naming the x axis
 plt.xlabel('Profit (Expected Potential Score)')
 # naming the y axis
